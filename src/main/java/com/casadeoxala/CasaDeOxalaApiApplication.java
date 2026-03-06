ff466ded9c7ffec0f4d4f9c88f0eecd3f9b4ad0b
wellbenicio <wellynton.benicio@gmail.com>
2026-03-06 14:15:48 -0300

    Estrutura inicial.

---
+++ src/main/java/com/casadeoxala/CasaDeOxalaApiApplication.java
@@ -0,0 +1,13 @@
+package com.casadeoxala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaDeOxalaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaDeOxalaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casadeoxala/CasaDeOxalaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casadeoxala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDeOxalaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
