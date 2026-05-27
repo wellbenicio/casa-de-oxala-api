2858014e2922582d5d394047ab417d382bb9c9ff
wellbenicio <wellynton.benicio@gmail.com>
2026-05-26 22:41:09 -0300

    test: adicionar asserções explícitas no teste de inicialização do contexto

--- src/test/java/com/casadeoxala/CasaDeOxalaApiApplicationTests.java
+++ src/test/java/com/casadeoxala/CasaDeOxalaApiApplicationTests.java
@@ -1,15 +1,28 @@
 package com.casadeoxala;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class CasaDeOxalaApiApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(applicationContext);
+	}
+
+	@Test
+	void applicationBeanExists() {
+		assertNotNull(applicationContext.getBean(CasaDeOxalaApiApplication.class));
 	}
 
 }
